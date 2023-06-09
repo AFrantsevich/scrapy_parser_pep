d96330539d3db447ad3ac78d6efcb572bd599828
unknown <Andrey.Frantsevich@gmail.com>
2023-06-09 09:18:51 +0300

    Ver.2

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,30 +1,25 @@
 import datetime
-
+from pathlib import Path
 
 DT_FORMAT = '%Y-%m-%d_%H-%M-%S'
-date_now = datetime.datetime.now()
-BASE_DIR = f'{"results/"}'
-
-
-result = {}
+BASE_DIR = Path(__file__).parents[1]
 
 
 class PepParsePipeline:
     def open_spider(self, spider):
-        ...
+        self.counter_status = {}
 
     def process_item(self, item, spider):
-        if item['status'] in result:
-            result[item['status']] += 1
-        else:
-            result[item['status']] = 1
+        item['status'] = self.counter_status.get(
+            item['status'], 0) + 1
         return item
 
     def close_spider(self, spider):
-        with open(f'{BASE_DIR}/'
-                  f'status_summary_{date_now.strftime(DT_FORMAT)}.csv',
+        with open(f'{BASE_DIR}/results/'
+                  f'status_summary_'
+                  f'{datetime.datetime.now().strftime(DT_FORMAT)}.csv',
                   mode='w', encoding='utf-8') as f:
             f.write('Статус,Количество\n')
-            for status, value in result.items():
+            for status, value in self.counter_status.items():
                 f.write(f'{status}, {value}\n')
-            f.write(f'Total,{sum(result.values())}\n')
+            f.write(f'Total,{sum(self.counter_status.values())}\n')
